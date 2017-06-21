cb236946d196203d8c0b01b5f12fcfee1c0cb3b0
PNNutkung <pipatpol.t@ku.th>
2017-06-21 11:15:11 +0900

    Press sign up go to signup page

--- src/components/login/RegisterPart.jsx
+++ src/components/login/RegisterPart.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import RaisedButton from 'material-ui/RaisedButton'
 
 export default class RegisterPart extends Component {
-  
   render() {
     return (
       <div>
-        <RaisedButton label="Sign up" className='login-input-btn' fullWidth={true} onClick={this.loginHandle} />
+        <RaisedButton label="Sign up" className='login-input-btn' fullWidth={true} href="/signup" />
       </div>
     )
   }
