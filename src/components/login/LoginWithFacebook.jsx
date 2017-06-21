fd90552608b2c1423617eb5dfd21eaa2c441e072
PNNutkung <pipatpol.t@ku.th>
2017-06-21 11:55:38 +0900

    Create LoginWithFacebook Component

---
+++ src/components/login/LoginWithFacebook.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LoginWithFacebook extends Component {
+  render() {
+    return (
+      <div classID='login-with-facebook'>
+        
+      </div>
+    )
+  }
+}
