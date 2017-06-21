832b8cbd3b6ddf1a14f0575c2c1aa9938d101adf
PNNutkung <pipatpol.t@ku.th>
2017-06-21 10:42:49 +0900

    Add login icon

--- src/components/login/LoginInputs.jsx
+++ src/components/login/LoginInputs.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
+import {Card, CardActions, CardTitle, CardText} from 'material-ui/Card'
+import {grey500} from 'material-ui/styles/colors'
+import AccountCircle from 'material-ui/svg-icons/action/account-circle'
 import TextField from 'material-ui/TextField'
-import RaisedButton from 'material-ui/RaisedButton';
+import RaisedButton from 'material-ui/RaisedButton'
+import RegisterPart from './RegisterPart'
 import './logininputs.css'
+
+const iconStyles = {
+  width: '100%',
+  height: 'auto'
+}
 
 export default class LoginInputs extends Component {
   constructor(props) {
@@ -25,9 +34,20 @@ export default class LoginInputs extends Component {
   render() {
     return (
       <div id='login-inputs-section'>
-          <TextField floatingLabelText='Username' name='username' type='text' value={this.state.username} onChange={this.handleChange} fullWidth={true} />
-          <TextField floatingLabelText='Password' name='password' type='password' value={this.state.password} onChange={this.handleChange} fullWidth={true} />
-          <RaisedButton label="Login" fullWidth={true} onClick={this.loginHandle}/>
+        <Card>
+          <CardTitle title="Log in to Mizuyari" />
+          <div id='account-icon'>
+            <AccountCircle style={iconStyles} color={grey500} />
+          </div>
+          <CardText>
+            <TextField floatingLabelText='Username' name='username' type='text' value={this.state.username} onChange={this.handleChange} fullWidth={true} />
+            <TextField floatingLabelText='Password' name='password' type='password' value={this.state.password} onChange={this.handleChange} fullWidth={true} />
+          </CardText>
+          <CardActions>
+            <RaisedButton label="Log in" className='login-input-btn' fullWidth={true} onClick={this.loginHandle} primary={true} />
+          </CardActions>
+        </Card>
+        <RegisterPart />
       </div>
     )
   }
