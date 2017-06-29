b63e90db7629561fa3a4eda2fd6e4b2d8423bf7c
PNNutkung <pipatpol.t@ku.th>
2017-06-29 14:30:15 +0900

    Add new icons

--- src/components/menu/MenuPage.jsx
+++ src/components/menu/MenuPage.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import { storageRef } from '../../config/firebase'
 
-const cardMenuCheckInStyle = {
-  background: `url(${storageRef.child('menu/checkin-card-image.jpg').getDownloadURL()}) center / cover`
-}
+// const cardMenuCheckInStyle = {
+//   background: `url(${storageRef.child('menu/checkin-card-image.jpg').getDownloadURL()}) center / cover`
+// }
 
 class MenuPage extends Component {
   checkinHandler = () => {
@@ -15,7 +15,7 @@ class MenuPage extends Component {
     return (
       <div>
         <div className='mdl-cell mdl-cell--12-col-desktop mdl-cell--12-col-tablet mdl-cell--12-col-phone'>
-          <div className='mdl-card mdl-shadow--2dp' style={cardMenuCheckInStyle} onClick={this.checkinHandler}>
+          <div className='mdl-card mdl-shadow--2dp' onClick={this.checkinHandler}>
             <div className='mdl-card__title mdl-card--expand'></div>
             <div className='mdl-card__actions'>
               <span className='demo-card-image__filename'>Check-in</span>
