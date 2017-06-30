f80f93d8399eb201204b52773cbd8cfaf8f8a6e8
PNNutkung <pipatpol.t@ku.th>
2017-06-30 10:38:02 +0900

    Add weather card

--- src/components/menu/MenuPage.jsx
+++ src/components/menu/MenuPage.jsx
@@ -6,13 +6,19 @@ class MenuPage extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      checkinCardImage: ''
+      checkinCardImage: '',
+      weatherForecastCardImage: ''
     }
     this.getImage('checkinCardImage')
+    this.getImage('weatherForecastCardImage')
   }
 
   checkinHandler = () => {
     console.log('Check in click')
+  }
+
+  forecastHandler = () => {
+    console.log('Weather forecast click')
   }
 
   getImage = (image) => {
@@ -29,8 +35,18 @@ class MenuPage extends Component {
       height: '200px',
       color: '#fff'
     }
+    let weatherForecastCardCheckinStyle = {
+      background: `url(${this.state.weatherForecastCardImage}) center / cover`,
+      height: '200px',
+      color: '#fff'
+    }
     return (
       <div className='mdl-cell mdl-cell--8-col-desktop mdl-cell--12-col-tablet mdl-cell--12-col-phone'>
+        <div className='mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col-desktop mdl-cell--12-col-tablet mdl-cell--12-col-phone' onClick={this.forecastHandler}>
+          <div className="mdl-card__title" style={weatherForecastCardCheckinStyle}>
+            <h2 className="mdl-card__title-text">Weather Forecast</h2>
+          </div>
+        </div>
         <div className='mdl-card mdl-shadow--2dp mdl-cell mdl-cell--12-col-desktop mdl-cell--12-col-tablet mdl-cell--12-col-phone' onClick={this.checkinHandler}>
           <div className="mdl-card__title" style={menuCardCheckinStyle}>
             <h2 className="mdl-card__title-text">Check-in</h2>
