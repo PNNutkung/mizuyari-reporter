5daad6f23d6e3cf4068b805583a734b5de543e77
PNNutkung <pipatpol.t@ku.th>
2017-07-06 21:16:36 +0900

    Prevent check-in twice per day

--- src/components/checkin/CheckInPage.jsx
+++ src/components/checkin/CheckInPage.jsx
@@ -5,7 +5,26 @@ import { dbRef } from '../../config/firebase'
 export default class CheckInPage extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      lastDate: null
+    }
     this.checkInHandler = this.checkInHandler.bind(this)
+    this.getTodayCheckins = this.getTodayCheckins.bind(this)
+  }
+
+  componentDidMount () {
+    this.getTodayCheckins()
+  }
+
+  getTodayCheckins () {
+    dbRef.child('checkins')
+    .orderByChild('date')
+    .limitToLast(1)
+    .on('value', snapshot => {
+      this.setState({
+        lastDate: Object.values(snapshot.val())[0].date
+      })
+    })
   }
 
   checkInHandler () {
@@ -17,12 +36,18 @@ export default class CheckInPage extends Component {
   }
 
   render () {
+    let lastDate = new Date(this.state.lastDate)
+    let alreadyWaterThePlant = (
+      lastDate.getFullYear() === (new Date()).getFullYear()
+      && lastDate.getMonth() === (new Date()).getMonth()
+      && lastDate.getDate() === (new Date()).getDate()
+    )
     return (
       <div className='check-in-page'>
         <p>Hello {this.props.userInfo.lastname} {this.props.userInfo.firstname} sama.</p>
         <Button
           bsStyle='primary'
-          disabled={!this.props.userInfo}
+          disabled={!this.props.userInfo || alreadyWaterThePlant }
           block
           onClick={this.checkInHandler}
         >
