f05cdde9c6dc161bd0bfe6c1648c4905beea99e2
PNNutkung <pipatpol.t@ku.th>
2017-07-08 17:48:25 +0900

    Add future forecast

--- src/components/weather/WeatherForecastPage.jsx
+++ src/components/weather/WeatherForecastPage.jsx
@@ -16,13 +16,14 @@ export default class WeatherForecastPage extends Component {
     axios.get(apixu.FORECAST_URL, {
       params: {
         key: apixu.API_KEY,
-        q: apixu.LOCATION
+        q: apixu.LOCATION,
+        days: 8
       }
     })
       .then((res) => {
         this.setState({
           currentWeather: res.data.current,
-          forecastWeather: res.data.forecast
+          forecastWeather: res.data.forecast.forecastday
         })
       })
       .catch((err) => {
@@ -32,8 +33,28 @@ export default class WeatherForecastPage extends Component {
   }
 
   render () {
+    let days = ['日', '月', '火', '水', '木', '金', '土']
+    let future = []
+    if (this.state.forecastWeather) {
+       this.state.forecastWeather.forEach(item => {
+        future.push(
+          <div className='oneday' key={item.date_epoch}>
+            <div className='date'>{days[(new Date(Number(String(item.date_epoch)+'000'))).getDay()]}</div>
+            <div className='icon'>
+              <img className='icon' src={`https:${item.day.condition.icon}`} alt='Weather icon' />
+            </div>
+            <div className='temp-high'>
+              <span className='value'>{item.day.maxtemp_c}</span>°
+            </div>
+            <div className='temp-low'>
+              <span className='value'>{item.day.mintemp_c}</span>°
+            </div>
+          </div>
+        )
+      })
+    }
     return (
-      <div>
+      <div id='weather-forecast-page'>
         {
           this.state.currentWeather == null
           ? (
@@ -43,19 +64,24 @@ export default class WeatherForecastPage extends Component {
           )
           : 
           (
-            <div>
-              <img src={`https:${this.state.currentWeather.condition.icon}`} alt="current weather icon"/>
-              <h2>
-                {(new Date(Number(String(this.state.currentWeather.last_updated_epoch)+'000'))).toDateString()}<br/>
-                {(new Date(Number(String(this.state.currentWeather.last_updated_epoch)+'000'))).toLocaleTimeString()}
-              </h2>
-              <h3>{this.state.currentWeather.text}</h3>
-              <p>
-                Current temperature: {this.state.currentWeather.temp_c}°C<br/>
-                Feels like: {this.state.currentWeather.feelslike_c}°C<br/>
-                Pressure: {this.state.currentWeather.pressure_mb} mBar<br/>
-                Humidity: {this.state.currentWeather.humidity}%<br/>
-              </p>
+            <div className='weather-forecast'>
+              <div>
+                <img src={`https:${this.state.currentWeather.condition.icon}`} alt="current weather icon"/>
+                <h2>
+                  {(new Date(Number(String(this.state.currentWeather.last_updated_epoch)+'000'))).toDateString()}<br/>
+                  {(new Date(Number(String(this.state.currentWeather.last_updated_epoch)+'000'))).toLocaleTimeString()}
+                </h2>
+                <h3>{this.state.currentWeather.text}</h3>
+                <p>
+                  Current temperature: {this.state.currentWeather.temp_c}°C<br/>
+                  Feels like: {this.state.currentWeather.feelslike_c}°C<br/>
+                  Pressure: {this.state.currentWeather.pressure_mb} mBar<br/>
+                  Humidity: {this.state.currentWeather.humidity}%<br/>
+                </p>
+              </div>
+              <div className='future'>
+                {future}
+              </div>
             </div>
           )
         }
