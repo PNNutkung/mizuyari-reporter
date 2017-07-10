1bdfa942ea6e112408b9e97130cc27bbb83e4fc4
PNNutkung <pipatpol.t@ku.th>
2017-07-10 11:16:14 +0900

    edit layout

--- src/components/menu/MenuPage.jsx
+++ src/components/menu/MenuPage.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { storageRef } from '../../config/firebase'
 import WeatherForecastComponent from './WeatherForecastComponent'
+import './MenuPage.css'
 
 class MenuPage extends Component {
   constructor (props) {
@@ -27,32 +28,38 @@ class MenuPage extends Component {
       color: 'inherit',
       textDecoration: 'none'
     }
+    let cardLink = {
+      textDecoration: 'none'
+    }
     let menuCardCheckinStyle = {
       background: `url(${this.state.checkinCardImage}) center / cover`,
+      width: '100%',
       height: '200px',
-      color: '#fff'
+      color: '#fff',
+      textDecoration: 'none'
     }
     let wateringLogsCardImageStyle = {
       background: `url(${this.state.wateringLogsCardImage}) center / cover`,
       height: '200px',
-      color: '#fff'
+      color: '#fff',
+      textDecoration: 'none'
     }
     return (
-      <div className='mdl-cell mdl-cell--8-col-desktop mdl-cell--12-col-tablet mdl-cell--12-col-phone'>
+      <div id='menu-page'>
         <Link to='/weather' style={style}>
           <WeatherForecastComponent />
         </Link>
-        <Link to='/checkin'>
-          <div className='card' onClick={this.checkinHandler}>
-            <div className="mdl-card__title" style={menuCardCheckinStyle}>
-              <h2 className="mdl-card__title-text">Check-in</h2>
+        <Link to='/checkin' style={cardLink}>
+          <div className='card-menu-page' onClick={this.checkinHandler}>
+            <div className='card-detail' style={menuCardCheckinStyle}>
+              <h2 className='card-title'>Check-in</h2>
             </div>
           </div>
         </Link>
-        <Link to='/logs'>
-          <div className='card' onClick={this.logHandler}>
-            <div className="mdl-card__title" style={wateringLogsCardImageStyle}>
-              <h2 className="mdl-card__title-text">Watering Logs</h2>
+        <Link to='/logs' style={cardLink}>
+          <div className='card-menu-page' onClick={this.logHandler}>
+            <div className='card-detail' style={wateringLogsCardImageStyle}>
+              <h2 className='card-title'>Watering Logs</h2>
             </div>
           </div>
         </Link>
