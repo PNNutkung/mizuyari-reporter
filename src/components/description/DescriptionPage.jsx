5a4adc77f87d1f5d768ba4d5fc54b090456d10f1
PNNutkung <pipatpol.t@ku.th>
2017-07-13 15:13:22 +0900

    Add text to description page

--- src/components/description/DescriptionPage.jsx
+++ src/components/description/DescriptionPage.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { Jumbotron, Button } from 'react-bootstrap'
 
 export default class DescriptionPage extends Component {
   render() {
-    console.log(this.props)
     return (
-      <div>
-        DescriptionPage
-        <br/>
-      </div>
+      <Jumbotron>
+        <h1>Mizuyari reporter</h1>
+        <p>
+          Shin has to operate his backbone. He cannot watering the plants by himself. This application will help Shin to know his plants are watered by his friends.
+        </p>
+        <p>
+          <Link to='/menu'>
+            <Button bsStyle='primary'>Let's start!</Button>
+          </Link>
+        </p>
+      </Jumbotron>
     )
   }
 }
